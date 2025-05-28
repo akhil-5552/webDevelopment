721d0eaf060d2c3e44dc4eccd74cc6232d0370ce
akhil-5552 <159676694+akhil-5552@users.noreply.github.com>
2025-05-28 20:35:06 +0530

    Create server.js

---
+++ Student Details Form/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const Student = require("./model/Student")
+
+const path = require('path')
+const app = express()
+const port = 5000
+
+mongoose.connect("mongodb://localhost:27017/studentDB")
+
+app.set('view engine' , 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/",async(req,res) => {
+    const students = await Student.find()
+    res.render('index',{students})
+})
+
+app.post('/save', async(req,res) => {
+    const {rollno, name, degree, city} = req.body
+    const students = new Student({rollno, name, degree, city})
+    await students.save()
+    res.redirect('/')
+})
+
+app.post('/delete/:id', async(req, res)=> {
+    const studentID = req.params.id
+    await Student.findByIdAndDelete(studentID.trim())
+    res.redirect('/')
+})
+
+app.listen((port),() =>{console.log(`server running on :${port}`)} )
